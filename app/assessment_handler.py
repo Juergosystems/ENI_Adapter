b1c7c3f62c3e9a7a554f1b2a04b48dffd07482ea
Jürg Käser <juerg.kaeser@abaqon.com>
2025-03-07 14:01:40 +0100

    extended assessment handler

--- app/assessment_handler.py
+++ app/assessment_handler.py
@@ -1,16 +1,40 @@
 class AssessmentHandler:
+    from flask import current_app as app
+    from app import config as cfg
+    
     def __init__(self, message):
+        self.message_id = message["id"]
+        self.message_time = message["time"]["delivered"]
+        self.message_type = message["type"]
+        
         self.assessment_id = message["data"]["object"]["id"]
+
         self.assessment_before = message["data"]["objectBefore"]
+        if self.assessment_before is not None:
+            self.state_before = message["data"]["objectBefore"]["state"]
+            self.score_before = message["data"]["objectBefore"]["score"]
+            self.docCount_before = message["data"]["objectBefore"]["docCount"]
+            self.owner_before = message["data"]["objectBefore"]["security"]["owners"][0]
+            self.assignee_before = message["data"]["objectBefore"]["assignedUsers"][0]["id"]
+            self.status_before = message["data"]["objectBefore"]["currentStatus"]["code"]
+        
         self.assessment_after = message["data"]["objectAfter"]
-        self.message = message
+        if self.assessment_after is not None:
+            self.state_after = message["data"]["objectAfter"]["state"]
+            self.score_after = message["data"]["objectAfter"]["score"]
+            self.docCount_after = message["data"]["objectAfter"]["docCount"]
+            self.owner_after = message["data"]["objectAfter"]["security"]["owners"][0]
+            self.assignee_after = message["data"]["objectAfter"]["assignedUsers"][0]["id"]
+            self.status_after = message["data"]["objectAfter"]["currentStatus"]["code"]
 
     def routing(self):
         if self.assessment_before is None:
             return self.new_assessment()
         return
 
-    def new_object(self):
+    def new_assessment(self):
+        if self.state_after == cfg.state.open:
+            return
         return
     
     def new_assignee(self):
