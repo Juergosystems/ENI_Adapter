ec37f3a30de039bfee8377f8a877f69152e36c62
Jürg Käser <juerg.kaeser@abaqon.com>
2025-03-19 17:43:24 +0100

    added error traceback

--- app/error_handler.py
+++ app/error_handler.py
@@ -5,6 +5,6 @@ import traceback
 errors = Blueprint("errors", __name__)
 
 @errors.app_errorhandler(Exception)
-def server_error(e):
+def error_handler(e):
     app.logger.error(f"An internal error occurred in the handling of the ENI message. {traceback.format_exc()}")
     return Response(f"An internal error occurred in the handling of the ENI message.", status=500)
